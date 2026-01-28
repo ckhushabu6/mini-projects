ab18385e6e8368abc80b7a5dccf012ea56ed0cfb
khushbu chauhan <ckhushabu6@gmail.com>
2026-01-28 23:59:14 +0530

    image_Gallery mini project

--- project2-prime-checker/script.js
+++ project2-prime-checker/script.js
@@ -1,25 +1,28 @@
-let input = document.getElementById("numberInpurt");
-let inputBtn = document.getElementById("checkBtN:");
+let input = document.getElementById("numberInput");
+let inputBtn = document.getElementById("checkBtN");
 let result = document.getElementById("result");
 function isprime(input){
-if(input.value === "" || isNaN(input.value)){
-    result.innerHTML = "please enter valid number";
-    return;
-}
-if(input.value < 2){
-    result.innerHTML= "please enter number more than one";
-    return;
-}
-else{
-    for(i = 2 ; i< input.value/2 ; i++){
-        if(input.value % 2==0){
-            result.innerHTML=`${input.value} is not a prime number`;
-            return ;
+
+    
+    if(input.value.trim() === "" || isNaN(input.value)){
+        result.innerHTML = "please enter valid number";
+        return;
+    }
+    let num = Number(input.value);
+    if(num < 2){
+        result.innerHTML= "please enter number more than one";
+        return;
+    }
+    else{
+        for(let i = 2 ; i <= Math.sqrt(num) ; i++){
+            if(num % i == 0){
+                result.innerHTML=`${num} is not a prime number`;
+                return ;
+            }
+            
         }
         
+        result.innerHTML = `${num} is a prime number`;
     }
-    result.innerHTML = `${input.value} is a prime number`;
-    return;
 }
-}
-inputBtn.addEventListener("click" ,isprime());
+inputBtn.addEventListener("click", isprime);
