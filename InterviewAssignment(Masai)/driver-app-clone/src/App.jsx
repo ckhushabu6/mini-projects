925c8ba03a5a13e2a54183a37fc4c8af2d962267
khushbu chauhan <ckhushabu6@gmail.com>
2026-05-05 23:46:57 +0530

    driver-app-clone

---
+++ InterviewAssignment(Masai)/driver-app-clone/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import About from './components/About'
+import Contect from './components/Contect'
+import Feature from './components/Feature'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Feature/>
+    <Contect/>
+    <Footer/>
+     <h1>Drive Smart. Connect Better.</h1>
+    </>
+  )
+}
+
+export default App
